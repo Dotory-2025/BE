3946f8385950ffe82b7ea66800bcd671e0905706
jeongabae <gabea52@gmail.com>
2026-01-02 15:30:21 +0900

    add : 공통 응답/에러 코드 추가

---
+++ src/main/java/com/dotoryteam/dotory/global/common/exception/ApiException.java
@@ -0,0 +1,14 @@
+package com.dotoryteam.dotory.global.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiException extends RuntimeException {
+
+    public final HttpStatus status;
+
+
+    public ApiException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+}
